29197fe05814b7e62e936d8debc11f05e560a877
saini4 <sainihemantsingh@gmail.com>
2024-09-21 13:10:53 -0700

    Update Array_Operations.cpp

--- Array_Operations.cpp
+++ Array_Operations.cpp
@@ -1,59 +1,53 @@
-
 #include <iostream>
 
-class MyArray
-{
+class MyArray {
 public:
-    void deleteAt(int arr[], int &size, int index)
-    {
-        if (index < 0 || index >= size)
-        {
+    void deleteAt(int arr[], int &size, int index) {
+        if (index < 0 || index >= size) {
             std::cout << "Index out of bounds!" << std::endl;
             return;
         }
-        for (int i = index; i <= size; i++)
-        {
+        for (int i = index; i < size - 1; i++) {
             arr[i] = arr[i + 1];
         }
         size--;
     }
 
-    void insertAt(int arr[], int element, int &size, int index)
-    {
-        if (index < 0 || index > size)
-        {
+    void insertAt(int arr[], int element, int &size, int index) {
+        if (index < 0 || index > size) {
             std::cout << "Index out of bounds!" << std::endl;
             return;
         }
-        for (int i = size; i >= index; i--)
-        {
-            arr[i + 1] = arr[i];
+        for (int i = size; i > index; i--) {
+            arr[i] = arr[i - 1];
         }
         arr[index] = element;
         size++;
     }
-    void printArr(int arr[], int size)
-    {
-        for (int i = 0; i < size; i++)
-        {
+
+    void printArr(int arr[], int size) {
+        for (int i = 0; i < size; i++) {
             std::cout << arr[i] << "\t";
         }
         std::cout << std::endl;
     }
 };
-int main()
-{
+
+int main() {
     int arr[15] = {44, 10, 20, 15, 40, 50, 45, 70, 21, 3};
     int size = 10;
     MyArray myArr;
-    myArr.printArr(arr, size);
 
+    myArr.printArr(arr, size);
+    
     int element = 37;
     int index;
+
     std::cout << "Enter the index for insertion:" << std::endl;
     std::cin >> index;
     myArr.insertAt(arr, element, size, index);
     myArr.printArr(arr, size);
+
     std::cout << "Enter the index for deletion:" << std::endl;
     std::cin >> index;
     myArr.deleteAt(arr, size, index);
